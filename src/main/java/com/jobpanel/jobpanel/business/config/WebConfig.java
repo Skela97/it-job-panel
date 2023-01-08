6d37d79b6871db81dbeec588995f9fb55a629abe
Skela97 <vlekovac@hotmail.com>
2023-01-08 14:48:54 +0100

    Fix CORS.

---
+++ src/main/java/com/jobpanel/jobpanel/business/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.jobpanel.jobpanel.business.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:4200","https://reqbin.com/")
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowedHeaders(CorsConfiguration.ALL);
+    }
+}
--- src/main/java/com/jobpanel/jobpanel/business/security/config/SecurityConfig.java
+++ src/main/java/com/jobpanel/jobpanel/business/security/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
